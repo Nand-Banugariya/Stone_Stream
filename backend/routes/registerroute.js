596c673b58b1fe73545bee44c51856e5a79983f9
Nand-Banugariya <152760341+Nand-Banugariya@users.noreply.github.com>
2024-10-04 21:41:58 +0530

    Update registerroute.js

--- backend/routes/registerroute.js
+++ backend/routes/registerroute.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const { registerUser, upload } = require('../controllers/registercontroller');
-
 const router = express.Router();
 
-// Register route with file upload handling
-router.post('/register', upload.single('photo'), registerUser); // Use multer middleware
+// Register route
+router.post('/register', upload.single('photo'), registerUser);
 
 module.exports = router;
